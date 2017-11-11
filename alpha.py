d30fb1fdf40ba8d7a58e027b18015e12d719770b
Leslie N McAdoo III <lmc3@umbc.edu>
2017-11-11 13:02:12 -0500

    Initial commit

---
+++ alpha.py
@@ -0,0 +1,33 @@
+# ALPHA Eleusis
+# CMSC 671
+# Dr. M
+# 
+#Authors:
+# Patrick Jenkins
+# Leslie McAdoo
+# Akshay Subramanya
+
+#This will hold the representation of all of the cards, 
+#   both legal and illegal, that have been played
+BOARD = []
+
+def scientist():
+    rule = None
+    cards_played = 0
+
+    while(not rule and cards_played < 200):
+
+
+        cards_played += 1
+
+    return rule
+
+def score():
+    board = boardState()
+
+
+def play(card):
+    pass
+
+def boardState():
+    return BOARD
